d56e008a0bfba6439b7956169a370b7231735a03
简 <2447746158@qq.com>
2024-04-10 16:58:29 +0800

    init with python env

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2305.491812.tar.gz";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default =
+          let
+            python = pkgs.python310;
+          in
+          pkgs.mkShell {
+            packages = [
+              (python.withPackages (ps: with ps; [
+                torch
+                torchvision            
+              ]))
+            ];
+          };
+      });
+    };
+}
