8c5a69f3089fb1e35bce733179632a0367c1943f
简 <2447746158@qq.com>
2024-04-13 19:13:11 +0800

    optimize MNIST-rust model.graident add forward_cached to avoid duplicated forward

--- MNIST/rust/src/model.rs
+++ MNIST/rust/src/model.rs
@@ -1,13 +1,14 @@
-use nalgebra::{SVector, SMatrix};
+use std::cell::Cell;
 
-type Vector<const N: usize> = SVector<f64, N>;
-type Matrix<const N: usize, const M: usize> = SMatrix<f64, N, M>;
+type Vector<const N: usize> = nalgebra::SVector<f64, N>;
+type Matrix<const N: usize, const M: usize> = nalgebra::SMatrix<f64, N, M>;
 
 pub trait Model<
     const N: usize, const M: usize
 > where
 {
     fn forward(&self, input: &Vector<N>) -> Vector<M>;
+    fn forward_cached(&self) -> Vector<M>;
     fn graident(&self, input: &Vector<N>) -> Matrix<M, N>;
     fn adjust(&mut self, mu: f64, gra: &Matrix<1, M>, input: &Vector<N>);
 }
@@ -16,16 +17,32 @@ pub trait Model<
 pub struct LinearModel<
     const N: usize, const M: usize
 > {
-    pub weights: SMatrix<f64, M, N>,
-    pub bias: SVector<f64, M>,
+    pub weights: Matrix<M, N>,
+    pub bias: Vector<M>,
+    cache: Cell<Vector<M>>
+}
+
+impl<const N: usize, const M: usize> LinearModel<N, M> {
+    pub fn new() -> Self {
+        Self {
+            weights: Matrix::zeros(),
+            bias: Vector::zeros(),
+            cache: Cell::new(Vector::zeros())
+        }
+    }
 }
 
 impl<const N: usize, const M: usize> Model<N, M> for LinearModel<N, M> {
-    fn forward(&self, input: &SVector<f64, N>) -> SVector<f64, M> {
-        self.weights * input + self.bias
+    fn forward(&self, input: &Vector<N>) -> Vector<M> {
+        self.cache.set(self.weights * input + self.bias);
+        self.forward_cached()
     }
 
-    fn graident(&self, _input: &SVector<f64, N>) -> SMatrix<f64, M, N> {
+    fn forward_cached(&self) -> Vector<M> {
+        self.cache.get()
+    }
+
+    fn graident(&self, _input: &Vector<N>) -> Matrix<M, N> {
         self.weights.clone()
     }
 
@@ -36,15 +53,30 @@ impl<const N: usize, const M: usize> Model<N, M> for LinearModel<N, M> {
 }
 
 
-pub struct ReLUModel<const N: usize> {}
+pub struct ReLUModel<const N: usize> {
+    cache: Cell<Vector<N>>
+}
+
+impl<const N: usize> ReLUModel<N> {
+    pub fn new() -> Self {
+        Self {
+            cache: Cell::new(Vector::zeros())
+        }
+    }
+}
 
 impl<const N: usize> Model<N, N> for ReLUModel<N> {
-    fn forward(&self, input: &SVector<f64, N>) -> SVector<f64, N> {
-        input.map(|x| x.max(0.0))
+    fn forward(&self, input: &Vector<N>) -> Vector<N> {
+        self.cache.set(input.map(|x| x.max(0.0)));
+        self.forward_cached()
     }
 
-    fn graident(&self, input: &SVector<f64, N>) -> SMatrix<f64, N, N> {
-        SMatrix::from_diagonal(
+    fn forward_cached(&self) -> Vector<N> {
+        self.cache.get()
+    }
+
+    fn graident(&self, input: &Vector<N>) -> Matrix<N, N> {
+        Matrix::from_diagonal(
             &input.map(|x| if x < 0.0 { 0.0 } else { 1.0 })
         )
     }
@@ -53,21 +85,36 @@ impl<const N: usize> Model<N, N> for ReLUModel<N> {
 }
 
 
-pub struct SoftMaxModel<const N: usize> {}
+pub struct SoftMaxModel<const N: usize> {
+    cache: Cell<Vector<N>>
+}
+
+impl<const N: usize> SoftMaxModel<N> {
+    pub fn new() -> Self {
+        Self {
+            cache: Cell::new(Vector::zeros())
+        }
+    }
+}
 
 impl<const N: usize> Model<N, N> for SoftMaxModel<N> {
-    fn forward(&self, input: &SVector<f64, N>) -> SVector<f64, N> {
+    fn forward(&self, input: &Vector<N>) -> Vector<N> {
         let exs = input.map(|x| x.exp());
         let s = exs.sum();
 
-        exs.map(|x| x / s)
+        self.cache.set(exs.map(|x| x / s));
+        self.forward_cached()
     }
 
-    fn graident(&self, input: &SVector<f64, N>) -> SMatrix<f64, N, N> {
+    fn forward_cached(&self) -> Vector<N> {
+        self.cache.get()
+    }
+
+    fn graident(&self, input: &Vector<N>) -> Matrix<N, N> {
         let exs = input.map(|x| x.exp());
         let s = exs.sum();
 
-        SMatrix::from_diagonal(
+        Matrix::from_diagonal(
             &input.map(|x| {
                 let ex = x.exp();
                 ex * (s - ex) / (s * s)
@@ -84,23 +131,35 @@ pub struct ConcatModel<
 > {
     m1: Box<dyn Model<N, M>>,
     m2: Box<dyn Model<M, O>>,
+    cache: Cell<Vector<O>>
+}
+
+impl<const N: usize, const M: usize, const O: usize> ConcatModel<N, M, O> {
+    pub fn new(m1: Box<dyn Model<N, M>>, m2: Box<dyn Model<M, O>>) -> Self {
+        Self { m1, m2, cache: Cell::new(Vector::zeros()) }
+    }
 }
 
 impl<
     const N: usize, const M: usize, const O: usize,
 > Model<N, O> for ConcatModel<N, M, O> {
     fn forward(&self, input: &Vector<N>) -> Vector<O> {
-        self.m2.forward(&self.m1.forward(input))
+        self.cache.set(self.m2.forward(&self.m1.forward(input)));
+        self.forward_cached()
+    }
+
+    fn forward_cached(&self) -> Vector<O> {
+        self.cache.get()
     }
 
     fn graident(&self, input: &Vector<N>) -> Matrix<O, N> {
-        let m2_input = self.m1.forward(input);
+        let m2_input = self.m1.forward_cached();
 
         self.m2.graident(&m2_input) * self.m1.graident(input)
     }
 
     fn adjust(&mut self, mu: f64, gra: &Matrix<1, O>, input: &Vector<N>) {
-        let m2_input = self.m1.forward(input);
+        let m2_input = self.m1.forward_cached();
         let m1_gra = gra * self.m2.graident(&m2_input);
 
         self.m2.adjust(mu, gra, &m2_input);
@@ -109,11 +168,11 @@ impl<
 }
 
 pub struct Loss<const N: usize, const M: usize> {
-    m: Box<dyn Model<N, M>>
+    pub m: Box<dyn Model<N, M>>
 }
 
 impl<const N: usize, const M: usize> Loss<N, M> {
-    fn adjust(&mut self, mu: f64, ys: &Vector<M>, xs: &Vector<N>) {
+    pub fn adjust(&mut self, mu: f64, ys: &Vector<M>, xs: &Vector<N>) {
         let predict = self.m.forward(xs);
 
         self.m.adjust(mu, &(2.0 * (predict - ys)).transpose(), xs);
